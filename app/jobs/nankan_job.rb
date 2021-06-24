84209a57f657758e367835b6786459ef64147451
evitch <yuuta040208@gmail.com>
2021-06-24 21:27:12 +0900

    rerun機能を実装#5

--- app/jobs/nankan_job.rb
+++ app/jobs/nankan_job.rb
@@ -5,11 +5,11 @@ Rails.app_class.load_tasks
 class NankanJob < ActiveJob::Base
   def perform
     Rake::Task['crawler:nankan'].invoke
-  rescue OpenURI::HTTPError => e
+  rescue ScrapingTargetNotExistError
+    # finish
+  rescue StandardError => e
     p e
     sleep(60 * 30)
     NankanJob.perform_now
-  rescue ScrapingTargetNotExistError
-    # finish
   end
 end
